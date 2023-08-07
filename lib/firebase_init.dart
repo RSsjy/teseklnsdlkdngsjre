9aa1fb40cd14d39381eb3445bcc37ad4df886662
RSsjy <58123564+RSsjy@users.noreply.github.com>
2023-08-07 05:23:48 -0400

    Delete lib directory

--- lib/firebase_init.dart
+++
@@ -1,7 +0,0 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-
-void initializeFirebase() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-}
