b6aed59aafa10a2c016ce271201908416663cc92
RSsjy <58123564+RSsjy@users.noreply.github.com>
2023-08-07 05:46:59 -0400

    Delete lib directory

--- lib/collection_page.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'collection_page_state.dart';
-
-class CollectionPage extends StatefulWidget {
-  const CollectionPage({Key? key}) : super(key: key);
-
-  @override
-  CollectionPageState createState() => CollectionPageState();
-}
