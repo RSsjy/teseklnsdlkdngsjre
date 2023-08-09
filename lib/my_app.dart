7e0b97f57914a900a9dd3cf2551b65457f3fe45e
RSsjy <58123564+RSsjy@users.noreply.github.com>
2023-08-09 02:11:44 -0400

    Add files via upload

---
+++ lib/my_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+import 'collection_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Inv App',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+        '/collection': (context) => const CollectionPage(),
+      },
+    );
+  }
+}
